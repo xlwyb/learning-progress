42cce0daa7371e91afec746635023d5d1d4f5d50
leaf north <120167469+xlwyb@users.noreply.github.com>
2025-10-27 17:00:40 +0800

    Add files via upload

---
+++ C Primer Plus/practise4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int toes = 10;
+    printf ("toes = %d\n", toes);
+    printf ("twice toes is %d\n", 2*toes);
+    printf ("double twice is %d\n", toes * toes);
+    return 0;
+}
