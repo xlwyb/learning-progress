0bc89de255e72788c603901ef021322434183d35
leaf north <120167469+xlwyb@users.noreply.github.com>
2025-10-27 17:06:19 +0800

    Add files via upload

---
+++ C Primer Plus/practise5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void smile();
+int main(void)
+{
+    smile();
+    smile();
+    smile();
+    printf ("\n");
+    smile();
+    smile();
+    printf ("\n");
+    smile();
+    return 0;
+}
+void smile(void)
+{
+    printf ("Smile!");
+}
