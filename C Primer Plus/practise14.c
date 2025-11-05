2e474844e372c91dae6fc9a9e09356ffe5cfe129
leaf north <120167469+xlwyb@users.noreply.github.com>
2025-11-05 22:10:23 +0800

    Add files via upload

---
+++ C Primer Plus/practise14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#define Q "\"His Hamlet was funny without being vulgar.\""
+
+int main()
+{
+    printf ("He sold the painting for $%2.2f\n", 2.345e2);
+    printf ("%c%c%c\n", 'H', 105, '\41');
+    printf ("%s\nhas %zd characters.\n", Q, strlen(Q));
+    printf ("Is %2.2e the same as %2.2f?\n", 1201.0, 1201.0);
+    return 0;
+}
