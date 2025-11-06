bf13fbb224ddd58226b2c6af09b290c86f133198
leaf north <120167469+xlwyb@users.noreply.github.com>
2025-11-06 09:23:19 +0800

    Update practise15.c

--- C Primer Plus/practise15.c
+++ C Primer Plus/practise15.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    char firstname[20], lastname[20];
-    printf ("please enter your first name: ");
-    scanf ("%s", firstname);
-    printf ("please enter your last name: ");
-    scanf ("%s", lastname);
-    printf ("your name is %s,%s\n", firstname, lastname);
+    char name[20];
+    printf ("please enter your name: ");
+    scanf ("%s", name);
+    int len = strlen(name);
+    printf ("%s\n", name);
+    printf ("%20s\n", "\"name\"");
+    printf ("%-20s\n","\"name\"");
+    printf ("%*s\n",len+5,"\"name\"");
     return 0;
 }
