791655cc2a9e70fc328117fad1ef5058a6f9e562
leaf north <120167469+xlwyb@users.noreply.github.com>
2025-11-07 20:34:11 +0800

    Update 计算多项式e^x的n阶麦克劳伦公式.c

--- test/程序设计（一）-综合测试：控制语句(2025-11-07)/计算多项式e^x的n阶麦克劳伦公式.c
+++ test/程序设计（一）-综合测试：控制语句(2025-11-07)/计算多项式e^x的n阶麦克劳伦公式.c
@@ -8,14 +8,12 @@ int main() {
     scanf("%lf", &eps);
     double sum = 0;  
     double temp = 1; 
-    int i = 0; 
-    int k = 1;       
+    int i = 0;         
     while (fabs(temp) >= eps) 
-    {   
-        for (int j=i;j>=1;j--) k *= j;       
-        temp = pow(x,i)*pow(-1,i)/k;
-        sum += temp;
-        i++;
+    {
+        sum += temp;   
+        i++;        
+        temp *= (-x) / i;
     }
     sum += temp;
     printf("%.3lf\n", sum);
